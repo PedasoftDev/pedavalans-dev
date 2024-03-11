a415b43df45b022f5e8e0f257b9087575c249122
Yusuf Ali Selek <yusufaliselek41@gmail.com>
2024-03-11 15:49:09 +0300

    .

--- bios/src/client/pages/PolyvalenceUnit/Controllers/ReportPolyvalenceUnitList.tsx
+++ bios/src/client/pages/PolyvalenceUnit/Controllers/ReportPolyvalenceUnitList.tsx
@@ -153,7 +153,6 @@ export class ReportPolyvalenceUnitList extends UIController {
                                                         </IconButton>
                                                     }
                                                 </div>
-
                                             )
                                         ),
                                     ),
